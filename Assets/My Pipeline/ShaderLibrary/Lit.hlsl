a36267c0c67a9eb204106b5adb764f0a00b5a327
du776239125 <776239125@qq.com>
2019-11-17 15:46:55 +0800

    第三次提交

--- Assets/My Pipeline/ShaderLibrary/Lit.hlsl
+++ Assets/My Pipeline/ShaderLibrary/Lit.hlsl
@@ -2,25 +2,39 @@
 #define MYRP_LIT_INCLUDE
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Shadow/ShadowSamplingTent.hlsl"
+
 CBUFFER_START(UnityPerFrame)   //逐帧
-   float4x4 unity_MatrixVP;
+float4x4 unity_MatrixVP;
 CBUFFER_END
 
 CBUFFER_START(UnityPerDraw)  
-  float4x4 unity_ObjectToWorld;
-  float4 unity_LightIndicesOffsetAndCount;
-  float4 unity_4LightIndices0, unity_4LightIndices1;
+
+float4x4 unity_ObjectToWorld;
+float4 unity_LightIndicesOffsetAndCount;
+float4 unity_4LightIndices0, unity_4LightIndices1;
 CBUFFER_END
 
 //最大可见灯光数量 宏定义
 #define MAX_VISIBLE_LIGHTS 16 
 
 CBUFFER_START(_LightBuffer)
-  float4 _VisibleLightColors[MAX_VISIBLE_LIGHTS];  //灯光颜色
-  float4 _VisibleLightDirectionsOrPositions[MAX_VISIBLE_LIGHTS];  //灯光方向
-  float4 _VisibleLightAttenuations[MAX_VISIBLE_LIGHTS];  //点灯光的距离衰减
-  float4 _VisibleLightSpotDirections[MAX_VISIBLE_LIGHTS];
+
+float4 _VisibleLightColors[MAX_VISIBLE_LIGHTS]; //灯光颜色
+float4 _VisibleLightDirectionsOrPositions[MAX_VISIBLE_LIGHTS]; //灯光方向
+float4 _VisibleLightAttenuations[MAX_VISIBLE_LIGHTS]; //点灯光的距离衰减
+float4 _VisibleLightSpotDirections[MAX_VISIBLE_LIGHTS];
 CBUFFER_END
+
+CBUFFER_START(_ShadowBuffer)
+
+float4x4 _WorldToShadowMatrix;
+float _ShadowStrength;
+float4 _ShadowMapSize;
+CBUFFER_END
+
+TEXTURE2D_SHADOW(_ShadowMap);
+SAMPLER_CMP(sampler_ShadowMap);
 
 #define UNITY_MATRIX_M unity_ObjectToWorld
 
@@ -32,77 +46,104 @@ UNITY_INSTANCING_BUFFER_END(PerInstance)
 
 struct VertexInput
 {
-   float4 pos:POSITION;
-   float3 normal:NORMAL;
+    float4 pos : POSITION;
+    float3 normal : NORMAL;
    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct VertexOutput
 {
-   float4 clipPos:SV_POSITION;
-   float3 normal:TEXCOORD0;
-   float3 worldPos:TEXCOORD1;
-   float3 vertexLighting:TEXCOORD2;
+    float4 clipPos : SV_POSITION;
+    float3 normal : TEXCOORD0;
+    float3 worldPos : TEXCOORD1;
+    float3 vertexLighting : TEXCOORD2;
    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
-float3 DiffuseLight(int index,float3 normal,float3 worldPos)
+float ShadowAttenuation(float3 worldPos)
 {
-   float3 lightColor=_VisibleLightColors[index].rgb;
-   float4 lightPositionOrDirection=_VisibleLightDirectionsOrPositions[index];
-   float4 lightAttenuation=_VisibleLightAttenuations[index];
-   float3 spotDirection=_VisibleLightSpotDirections[index].xyz;
+    float4 shadowPos = mul(_WorldToShadowMatrix, float4(worldPos, 1.0));
+    shadowPos.xyz /= shadowPos.w;
+    float attenuation = SAMPLE_TEXTURE2D_SHADOW(_ShadowMap, sampler_ShadowMap, shadowPos.xyz);
+#if defined(_SHADOWS_SOFT)
+	  real tentWeights[9];
+	  real2 tentUVs[9];
+	  SampleShadow_ComputeSamples_Tent_5x5(_ShadowMapSize,shadowPos.xy,tentWeights,tentUVs);
+	  attenuation=0;
+	  for(int int=0;i<9;i++)
+	 {
+	   attenuation+=tentWeights[i]*SAMPLE_TEXTURE2D_SHADOW(_ShadowMap,sampler_ShadowMap,float3(tentUVs[i].xy,shadowPos));
+	 }
+#endif
+    return lerp(1, attenuation, _ShadowStrength);
+}
 
-   float3 lightVector=lightPositionOrDirection.xyz-worldPos*lightPositionOrDirection.w;
-   float3 lightDirection=normalize(lightVector);
-   float diffuse=saturate(dot(normal,lightDirection));
+float3 DiffuseLight
+    (
+    int index, float3 normal, float3 worldPos, float shadowAttenuation)
+{
+    float3 lightColor = _VisibleLightColors[index].rgb;
+    float4 lightPositionOrDirection = _VisibleLightDirectionsOrPositions[index];
+    float4 lightAttenuation = _VisibleLightAttenuations[index];
+    float3 spotDirection = _VisibleLightSpotDirections[index].xyz;
+
+    float3 lightVector = lightPositionOrDirection.xyz - worldPos * lightPositionOrDirection.w;
+    float3 lightDirection = normalize(lightVector);
+    float diffuse = saturate(dot(normal, lightDirection));
    //计算灯光范围衰减,在灯光外是暗的照不亮的
-   float rangeFade=dot(lightVector,lightVector)*lightAttenuation.x;
-   rangeFade=saturate(1-rangeFade*rangeFade);
-   rangeFade*=rangeFade;
+    float rangeFade = dot(lightVector, lightVector) * lightAttenuation.x;
+    rangeFade = saturate(1 - rangeFade * rangeFade);
+    rangeFade *= rangeFade;
    
-   float spotFade=dot(spotDirection,lightDirection);
-   spotFade=saturate(spotFade*lightAttenuation.z+lightAttenuation.w);
-   spotFade*=spotFade;
+    float spotFade = dot(spotDirection, lightDirection);
+    spotFade = saturate(spotFade * lightAttenuation.z + lightAttenuation.w);
+    spotFade *= spotFade;
 
-   float distanceSqr = max(dot(lightVector, lightVector), 0.00001);
+    float distanceSqr = max(dot(lightVector, lightVector), 0.00001);
 
-   diffuse*=spotFade*rangeFade/distanceSqr;
-   return diffuse*lightColor;
+    diffuse *= shadowAttenuation *
+    spotFade * rangeFade / distanceSqr;
+    return diffuse * lightColor;
 }
 
-VertexOutput LitPassVertex(VertexInput input)
+VertexOutput LitPassVertex
+    (VertexInput input)
 {
-   VertexOutput output;
-   float4 worldPos=mul(UNITY_MATRIX_M,float4(input.pos.xyz,1.0));
-   output.clipPos=mul(unity_MatrixVP,worldPos);
-   output.normal=mul((float3x3)UNITY_MATRIX_M,input.normal);
-   output.worldPos=worldPos.xyz;
-
-   return output;
+    VertexOutput output;
+    float4 worldPos = mul(UNITY_MATRIX_M, float4(input.pos.xyz, 1.0));
+    output.clipPos = mul(unity_MatrixVP, worldPos);
+    output.normal = mul((float3x3) UNITY_MATRIX_M, input.normal);
+    output.worldPos = worldPos.xyz;
+   //顶点光输出先设置为0
+    output.vertexLighting = 0;
+    for (int i = 4; i < min(unity_LightIndicesOffsetAndCount.y, 8); i++)
+    {
+        int lightIndex = unity_4LightIndices1[i - 4];
+        output.vertexLighting += DiffuseLight(lightIndex, output.normal, output.worldPos, 1);
+    }
+    return output;
 }
 
-float4 LitPassFragment(VertexOutput input) : SV_TARGET
+float4 LitPassFragment
+    (VertexOutput input) :
+    SV_TARGET
 {
-   UNITY_SETUP_INSTANCE_ID(input);
-   input.normal=normalize(input.normal);
-   float3 albedo=UNITY_ACCESS_INSTANCED_PROP(PreInstance,_Color).rgb;
+    UNITY_SETUP_INSTANCE_ID(input);
+    input.normal = normalize(input.normal);
+    float3 albedo = UNITY_ACCESS_INSTANCED_PROP(PreInstance, _Color).rgb;
    
-   float3 diffuseLight=0;
-   for(int i=0;i<min(unity_LightIndicesOffsetAndCount.y,4);i++)
-   {
-      int lightIndex=unity_4LightIndices0[i];
-      diffuseLight+=DiffuseLight(lightIndex,input.normal,input.worldPos);
-   }
-    for(int i=4;i<min(unity_LightIndicesOffsetAndCount.y,8);i++)
-   {
-      int lightIndex=unity_4LightIndices1[i-4];
-      diffuseLight+=DiffuseLight(lightIndex,input.normal,input.worldPos);
-   }
+    float3 diffuseLight = input.vertexLighting;
+    for (int i = 0; i < min(unity_LightIndicesOffsetAndCount.y, 4); i++)
+    {
+        int lightIndex = unity_4LightIndices0[i];
+        float shadowAttenuation = ShadowAttenuation(input.worldPos);
+        diffuseLight += DiffuseLight(lightIndex, input.normal, input.worldPos, shadowAttenuation);
+    }
+   
    //float3 diffuseLight=saturate(dot(input.normal,float3(0,1,0)));
 
-   float3 color=diffuseLight*albedo;
-   return float4(color,1);
+    float3 color = diffuseLight * albedo;
+    return float4(color, 1);
 }
 
 #endif //MYRP_LIT_INCLUDE
--- Assets/My Pipeline/Shaders/Lit.shader
+++ Assets/My Pipeline/Shaders/Lit.shader
@@ -20,5 +20,23 @@
             #include "../ShaderLibrary/Lit.hlsl"
             ENDHLSL
         }
+
+		 Pass{
+		    Tags{ "LightMode"="ShadowCaster"	}
+            HLSLPROGRAM   //HLSL程序
+            
+            #pragma target 3.5
+            //GPU instanceing
+            #pragma multi_compile_instancing
+            #pragma instancing_options assumeuniformscaling
+
+			#pragma multi_compile _ _SHADOWS_SOFT
+
+            #pragma vertex ShadowCasterPassVertex
+            #pragma fragment ShadowCasterPassFragment
+
+            #include "../ShaderLibrary/ShadowCaster.hlsl"
+            ENDHLSL
+        }
     }
 }
